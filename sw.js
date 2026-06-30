0179393970cb0b653e24bd13a6991495ef8006b4
rontogiannisgeorgios-13 <rontogiannisgeorgios@gmail.com>
2026-06-30 16:54:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'eae-mini-app-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './images/EAE_logo_EN.png',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => cachedResponse || fetch(event.request))
+  );
+});
